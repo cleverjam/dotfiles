f94068aaa7661d016b51fb3c1967197f0a42b953
Jaime Liz <lizjaime@gmail.com>
2020-12-18 12:20:30 -0600

    feat: icon config

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,15 @@ set fish_greeting
 
 _add_to_path /opt/homebrew/bin
 
-set -u tide_left_prompt_items pwd git_prompt newline prompt_char
-set -u tide_right_prompt_items status cmd_duration context jobs nvm virtual_env
-set -u tide_prompt_connection_icon '·'
-set -u tide_print_newline_before_prompt true
-set -u tide_prompt_connection_color 444444
-set -u tide_left_prompt_frame_color 444444
-set -u tide_right_prompt_frame_color 444444
+set -g tide_left_prompt_items pwd git_prompt newline prompt_char
+set -g tide_right_prompt_items status cmd_duration context jobs nvm virtual_env
+set -g tide_prompt_connection_icon '·'
+set -g tide_print_newline_before_prompt true
+set -g tide_prompt_connection_color 444444
+set -g tide_left_prompt_frame_color 444444
+set -g tide_right_prompt_frame_color 444444
+
+
+set -g tide_pwd_dir_icon
+set -g tide_pwd_home_icon
+set -g tide_cmd_duration_icon 
