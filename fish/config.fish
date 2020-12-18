2eb049fe988b896417ee993190c9edbc818143d5
Jaime Liz <lizjaime@gmail.com>
2020-12-18 12:05:00 -0600

    feat: configure prompt

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,11 @@
 set fish_greeting
 
 _add_to_path /opt/homebrew/bin
+
+set -u tide_left_prompt_items pwd git_prompt newline prompt_char
+set -u tide_right_prompt_items status cmd_duration context jobs nvm virtual_env
+set -u tide_prompt_connection_icon '·'
+set -u tide_print_newline_before_prompt true
+set -u tide_prompt_connection_color 444444
+set -u tide_left_prompt_frame_color 444444
+set -u tide_right_prompt_frame_color 444444
