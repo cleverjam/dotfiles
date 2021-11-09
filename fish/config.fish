77b7b6a9d9d55d05e1a48f55ab34cb8fa91dc11a
Jaime Liz <lizjaime@gmail.com>
2021-11-09 17:21:52 -0600

    fix: remove tmux and fish_variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,4 @@
-# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
-# only run in interactive (not automated SSH for example)
-if status is-interactive
-# don't nest inside another tmux
-and not set -q TMUX
-  # Adapted from https://unix.stackexchange.com/a/176885/347104
-  # Create session 'main' or attach to 'main' if already exists.
-  tmux new-session -A -s main
-end
+
 
 # Remove fish help message on every new session
 set fish_greeting
