a2c4b1af11c4d57d03cb0104d8aea01a6865e770
Jaime Liz <lizjaime@gmail.com>
2022-03-04 14:04:06 -0600

    fix: move init commands to install yaml

--- fish/config.fish
+++ fish/config.fish
@@ -8,16 +8,3 @@ source ~/.config/fish/aliases.fish
 
 _add_to_path /opt/homebrew/bin
 _add_to_path  /Users/cleverjam/.cargo/bin
-
-
-# x86 brew
-if test -e /usr/local/Caskroom
-    eval /usr/local/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-    source /usr/local/opt/asdf/libexec/asdf.fish
-end
-
-#Apple silicon brew
-if test -e /opt/homebrew/Caskroom/
-    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-    source /opt/homebrew/opt/asdf/libexec/asdf.fish
-end
