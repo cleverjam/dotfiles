2ed9a39010148b41a044557fc1d143be580012f1
Jaime Liz <jaime_liz@intuit.com>
2022-03-04 13:54:18 -0600

    fix: brew location changes depending on platform

--- fish/config.fish
+++ fish/config.fish
@@ -9,10 +9,15 @@ source ~/.config/fish/aliases.fish
 _add_to_path /opt/homebrew/bin
 _add_to_path  /Users/cleverjam/.cargo/bin
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
+# x86 brew
+if test -e /usr/local/Caskroom
+    eval /usr/local/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+    source /usr/local/opt/asdf/libexec/asdf.fish
+end
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
+#Apple silicon brew
+if test -e /opt/homebrew/Caskroom/
+    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+end
