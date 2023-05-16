12052931c5e242aae7f5734b9d8f147f6266248f
Jaime Liz <lizjaime@gmail.com>
2023-05-16 11:19:47 -0500

    feat: multicd expansion fn

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -4,3 +4,4 @@ alias k 'kubectl'
 alias cdc 'cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/'
 alias skip-tree 'git update-index --skip-worktree'
 alias unskip-tree 'git update-index --no-skip-worktree'
+abbr --add dotdot --regex '^\.\.+$' --function _multicd
---
+++ fish/functions/_multicd.fish
@@ -0,0 +1,3 @@
+function _multicd
+  echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
