fc29e5cf9df34435a516053e2f1852d3476a82f1
Jaime Liz <lizjaime@gmail.com>
2024-02-29 19:39:16 -0600

    remoe pinentry-mac

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -4,5 +4,4 @@ alias k 'kubectl'
 alias cdc 'cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/'
 alias skip-tree 'git update-index --skip-worktree'
 alias unskip-tree 'git update-index --no-skip-worktree'
-alias pinentry='pinentry-mac'
 abbr --add dotdot --regex '^\.\.+$' --function _multicd
